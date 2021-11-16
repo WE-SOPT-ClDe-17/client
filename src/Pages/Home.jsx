39d87aa42d8d04743db269e9f61c329a7004e311
JisuKim <jso_osj@naver.com>
2021-11-16 21:14:32 +0900

    chore: git ignore 추가, Cards컴포넌트 추가

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 import Footer from "../Components/Home/Footer";
+import Cards from "../Components/Home/Cards";
+// import ContentData from "../Lib/Constants/ContentData.json";
 
-const Text = styled.h1`
-  font-family: "Pretendard-Bold";
-  font-size: 2rem;
-  color: blue;
+const StyledRecommendBox = styled.div`
+  padding: 2.25rem;
 `;
 
-const Medium = styled.h1`
-  font-family: "Pretendard-Medium";
-  font-size: 2rem;
-`;
-
-const Bold = styled.h1`
+const StyledTitle = styled.h2`
   font-family: "Pretendard";
   font-size: 2rem;
+  line-height: 1.625rem;
   font-weight: 700;
 `;
 
 const Home = () => {
   return (
     <>
-      <Button>🔥 김 지 수 현 지 영 이 주 함 🔥</Button>
-      <Text>볼드체</Text>
-      <Medium>프리텐다드 미디엄</Medium>
-      <Bold>🔥 김지수 현지영 이주함 🔥 볼드</Bold>
+      <StyledRecommendBox>
+        <StyledTitle>2022 신년준비 위원회</StyledTitle>
+      </StyledRecommendBox>
+      <Cards />
       <Footer />
     </>
   );
@@ -33,11 +29,11 @@ const Home = () => {
 
 export default Home;
 
-const Button = styled.button`
-  color: ${({ theme: { colors } }) => colors.red1};
-  font-size: 3rem;
+// const Button = styled.button`
+//   color: ${({ theme: { colors } }) => colors.red1};
+//   font-size: 3rem;
 
-  @media ${({ theme: { device } }) => device.tablet} {
-    color: green;
-  }
-`;
+//   @media ${({ theme: { device } }) => device.tablet} {
+//     color: green;
+//   }
+// `;
