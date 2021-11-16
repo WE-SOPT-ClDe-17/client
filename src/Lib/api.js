a6e3fc5fc954431fc3482722165aa4efe48aa5ba
JisuKim <jso_osj@naver.com>
2021-11-16 21:11:02 +0900

    Init: 이미지파일, 더미데이터, json-server설치

--- src/Lib/api.js
+++ src/Lib/api.js
@@ -1,3 +1,8 @@
 import axios from "axios";
 
-export const client = axios.create({ baseURL: "http://localhost:3000" });
+export const client = axios.create({
+  baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
